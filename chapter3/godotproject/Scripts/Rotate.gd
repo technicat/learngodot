eeae4282f60ca997620c9da2b236fdec468ac117
Philip Chu <atlassian@technicat.com>
2021-09-07 22:32:42 -0700

    add speed property

--- chapter3/godotproject/Scripts/Rotate.gd
+++ chapter3/godotproject/Scripts/Rotate.gd
@@ -1,16 +1,9 @@
 extends Spatial
 
+export var speed = 1.0;
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate_y(delta);
+	rotate_y(delta*speed);
