b3455d9b0ff829b87fed3d5bbe8f3c40c606d836
Philip Chu <atlassian@technicat.com>
2021-09-10 08:43:21 -0700

    clean up scripts

--- chapter4/godotproject/Scripts/look_at.gd
+++ chapter4/godotproject/Scripts/look_at.gd
@@ -3,7 +3,3 @@ extends Spatial
 
 func _ready():
 	look_at(Vector3(0,0,0),Vector3(0,0,-1));
-
-
-func _process(delta):
-	look_at(Vector3(0,0,0),Vector3(0,0,-1));
--- chapter4/godotproject/Scripts/rotate_y.gd
+++ chapter4/godotproject/Scripts/rotate_y.gd
@@ -2,8 +2,5 @@ extends Spatial
 
 export var speed = 1.0;
 
-func _ready():
-	pass # Replace with function body.
-
 func _process(delta):
 	rotate_y(delta*speed);
